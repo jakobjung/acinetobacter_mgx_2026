adeaf2b8259d820d04443ae40b03118c3786b97d
Jakob <jakobjung@tutanota.com>
2026-03-16 14:42:14 +0100

    changed bash a bit

--- scripts/download_raw_reads.sh
+++ scripts/download_raw_reads.sh
@@ -5,10 +5,10 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --output=download_%j.log
 
-mkdir -p /home/jjung/acinetobacter_mgx_2026/data/fastq
+mkdir -p /vol/projects/jjung/acinetobacter_mgx_2026/data/fastq
 
 for CRA in CRA010962 CRA010735 CRA010639; do
-    wget -r -np -nd --continue -P /home/jjung/acinetobacter_mgx_2026/data/fastq/${CRA} \
+    wget -r -np -nd --continue -P /vol/projects/jjung/acinetobacter_mgx_2026/data/fastq/${CRA} \
          ftp://download.big.ac.cn/gsa/${CRA}/
 done
 
